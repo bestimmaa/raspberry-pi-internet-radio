89932fbe53dd50b79e1cc03848c60f7ca12a1767
Christoph Halang <christoph.halang@gmail.com>
2013-12-19 00:33:34 +0000

    fancy button support

--- led.py
+++ led.py
@@ -2,33 +2,75 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import subprocess
+from subprocess import check_output as qx
 
 buttonLastPress = datetime.datetime.now()
 
-def toogleRadio():
-	subprocess.call(["mpc", "toggle"])
+class Physical:
+	statusLED = 0
+	lastStatusUpdate = datetime.datetime.now()
+	def __init__(self ):
+		self.statusLED = 0
+		self.lastStatusUpdate = datetime.datetime.now()
+		self.setupGPIO()
 
+	def update(self):
+		self.updateStatusLED()
+
+	def setupGPIO(self):
+		self.cleanGPIO()
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(10, GPIO.OUT, 0)
+		GPIO.setup(8, GPIO.IN)
+	def cleanGPIO(self):
+		GPIO.cleanup()
+
+	def updateStatusLED(self):
+		status = qx(["mpc"])
+		now = datetime.datetime.now()
+		delta = now - self.lastStatusUpdate
+		if delta.total_seconds() > 0.5:
+			self.lastStatusUpdate = now
+			#print status
+			if "[playing]" in status:
+				GPIO.output(10,1)
+			else:
+				GPIO.output(10,0)
+
+
+
+
+
+def toogleRadio():
+	qx(["mpc", "toggle"])
+
+def stopRadio():
+	qx(["mpc", "stop"])
 
 try:
-
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(10, GPIO.OUT, 0)
-	GPIO.setup(8, GPIO.IN)
-	subprocess.call(["mpc", "stop"])
+	print "Starting Pifi..."
+	phy = Physical()
+	stopRadio()
 
 	while 1:
-		if GPIO.input(8):
+		phy.update()
+		if not GPIO.input(8):
 			now = datetime.datetime.now()
 			delta = now - buttonLastPress
 			if delta.total_seconds() > 0.4:
-				buttonLastPress = datetime.datetime.now()
-				print "Button pressed - " + str(datetime.datetime.now().time())
+				buttonLastPress = now
+				print "Play/Pause button pressed"
 				toogleRadio()
-			GPIO.output(10,1)
-		else:
-			GPIO.output(10,0)
-
+	phy.cleanGPIO();
+	qx(["mpc", "stop"])
+except Exception, e:
 	GPIO.cleanup()
+	qx(["mpc", "stop"])
+	print "Error",e
+	print "\nSooo error. No playback. Channels. Much Clean. Very Terminating. WOW."
 except:
 	GPIO.cleanup()
-	print "\n Cleaned all pins - Terminating..."
+	qx(["mpc", "stop"])
+	print "\nSooo unexpected. No playback. Channels. Much Clean. Very Terminating. WOW."
+
+
