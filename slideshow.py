474b46731e42ebbf50054d3e8a6314d288a3c078
Christoph Halang <christoph.halang@gmail.com>
2014-01-28 00:51:13 +0000

    added missing files from last commit :)

--- slideshow.py
+++ slideshow.py
@@ -3,17 +3,20 @@
  
 import os
 import random
- 
-img_folder="/home/pi/pics" # Anpassen, Ordner mit Pics darin
-                             # AM ENDE KEIN /
-walker = os.walk(img_folder,True,None)
-pics = [] # leer lassen
- 
-def give_random_pic(self):
-        for i in walker:
-                if len(i[2]) > 0:
-                        for j in i[2]:
-                                if j.split(".")[-1] == "png": # Pruefe ob die Endung .png ist
-                                        pics.append(i[0]+"/"+j)
-        rn = random.randrange(0,len(pics),1)
-        return pics[rn]
+
+class Slideshow:
+	img_folder=""
+	
+	def __init__(self):
+		self.img_folder="/home/pi/pics"
+		
+	def give_random_pic(self):
+		return self.img_folder+"/flower.png"
+		
+def main(textarg):
+	sw = Slideshow()
+	return sw.give_random_pic()
+		
+if __name__ == "__main__":
+	sw = Slideshow()
+	print sw.give_random_pic()
--- update_configuration.sh
+++ update_configuration.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "updating configuration"
+echo "updating configuration..."
+
+#kill lcd4linux process
+killall lcd4linux
 
 mydir="$(pwd)"
 
@@ -12,12 +15,23 @@ chmod 755 /usr/local/bin/chrisfm/chrisfm.py
 cp $mydir/chrisfmservice.sh /etc/init.d/chrisfmservice.sh
 chmod 755 /etc/init.d/chrisfmservice.sh
 
+cp $mydir/lcd4linuxservice.sh /etc/init.d/lcd4linuxservice.sh
+chmod 755 /etc/init.d/lcd4linuxservice.sh
+
 #let daemon run at default times
 update-rc.d chrisfmservice.sh defaults
+update-rc.d lcd4linuxservice.sh defaults
 
 #copy lcd4linux dpf configuration
 cp $mydir/lcd4linux.conf /etc/lcd4linux.conf
 chmod 600 /etc/lcd4linux.conf
 chown root.root /etc/lcd4linux.conf
 
+#copy python module for driving ldc4linux
+cp $mydir/slideshow.py /home/pi/python_modules/slideshow.py
+chown root:root /home/pi/python_modules/slideshow.py
+chmod 700 /home/pi/python_modules/slideshow.py 
 
+#restart chrisfm service and lcd4linux
+/etc/init.d/lcd4linuxservice.sh restart
+/etc/init.d/chrisfmservice.sh restart
