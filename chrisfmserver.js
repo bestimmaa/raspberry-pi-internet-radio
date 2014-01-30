db7dcc562d420008b22cbcafc264d9698586f834
Christoph Halang <christoph.halang@gmail.com>
2014-01-30 22:59:23 +0000

    added nodjs script for running a minimalistic API that hubot can access

---
+++ chrisfmserver.js
@@ -0,0 +1,44 @@
+/*
+
+Respond to HTTP GET requests http://URL.com/?radio=play&volume=100
+http://\/\?radio=(play|pause)
+*/
+var regex =  new RegExp("\/\?radio=(start|stop)");
+var http = require('http');
+http.createServer(function (req, res) {
+	console.log("Received request with url:"+req.url);
+	var result = regex.exec(req.url);
+	console.log("Found arguments: " + result);
+	if (result != null){
+		if (result[1] == "stop") {
+			console.log("We should stop the radio...");
+			var sys = require('sys')
+			var exec = require('child_process').exec;
+			function puts(error, stdout, stderr) {
+			 	sys.puts(stdout);
+			 	res.writeHead(200, {'Content-Type': 'text/plain'});
+  				res.end(stdout);
+			}
+			exec("mpc stop", puts);
+
+		};
+		if (result[1] == "start") {
+			console.log("We should start the radio...");
+			var sys = require('sys')
+			var exec = require('child_process').exec;
+			function puts(error, stdout, stderr) {
+			 	sys.puts(stdout);
+			 	res.writeHead(200, {'Content-Type': 'text/plain'});
+  				res.end(stdout);
+			}
+			exec("mpc play", puts);
+		};
+	}
+
+	else{
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+  			res.end('Hello World!');
+	}
+
+}).listen(8080, '192.168.178.26');
+console.log('Server running at http://192.168.178.26:8080/ and http://notanotherhoneypot.selfhost.eu');
