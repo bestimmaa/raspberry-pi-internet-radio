95a296723351961aca1ff6922fff472865cc4ee5
Christoph Halang <christoph.halang@gmail.com>
2014-01-31 01:07:33 +0000

    updated API server

--- chrisfmserver.js
+++ chrisfmserver.js
@@ -3,36 +3,43 @@
 Respond to HTTP GET requests http://URL.com/?radio=play&volume=100
 http://\/\?radio=(play|pause)
 */
-var regex =  new RegExp("\/\?radio=(start|stop)");
+var regex =  new RegExp("\/\??(([a-z]+)=([a-z]+|\d+))");
 var http = require('http');
 http.createServer(function (req, res) {
 	console.log("Received request with url:"+req.url);
 	var result = regex.exec(req.url);
 	console.log("Found arguments: " + result);
 	if (result != null){
-		if (result[1] == "stop") {
-			console.log("We should stop the radio...");
-			var sys = require('sys')
-			var exec = require('child_process').exec;
-			function puts(error, stdout, stderr) {
-			 	sys.puts(stdout);
-			 	res.writeHead(200, {'Content-Type': 'text/plain'});
-  				res.end(stdout);
-			}
-			exec("mpc stop", puts);
+		if (result.length == 4) {
+			if (result[2] == "radio" & result[3] == "stop") {
+				console.log("We should stop the radio...");
+				var sys = require('sys')
+				var exec = require('child_process').exec;
+				function puts(error, stdout, stderr) {
+				 	sys.puts(stdout);
+				 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	  				res.end(stdout);
+				}
+				exec("mpc stop", puts);
 
-		};
-		if (result[1] == "start") {
-			console.log("We should start the radio...");
-			var sys = require('sys')
-			var exec = require('child_process').exec;
-			function puts(error, stdout, stderr) {
-			 	sys.puts(stdout);
-			 	res.writeHead(200, {'Content-Type': 'text/plain'});
-  				res.end(stdout);
-			}
-			exec("mpc play", puts);
-		};
+			};
+			if (result[2] == "radio" & result[3] == "start") {
+				console.log("We should start the radio...");
+				var sys = require('sys')
+				var exec = require('child_process').exec;
+				function puts(error, stdout, stderr) {
+				 	sys.puts(stdout);
+				 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	  				res.end(stdout);
+				}
+				exec("mpc play", puts);
+			};
+		}
+		else{
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+	  				res.end("Bad arguments!");
+		}
+
 	}
 
 	else{
