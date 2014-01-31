16bc83b08c69034ee143449b8f51d3423e82a67d
Christoph Halang <christoph.halang@gmail.com>
2014-01-31 21:34:20 +0000

    updated API server

--- chrisfmserver.js
+++ chrisfmserver.js
@@ -1,9 +1,9 @@
 /*
 
 Respond to HTTP GET requests http://URL.com/?radio=play&volume=100
-http://\/\?radio=(play|pause)
+
 */
-var regex =  new RegExp("\/\??(([a-z]+)=([a-z]+|\d+))");
+var regex =  /\/\?(([a-z]+)=([a-z]+|\d+))/
 var http = require('http');
 http.createServer(function (req, res) {
 	console.log("Received request with url:"+req.url);
@@ -21,7 +21,6 @@ http.createServer(function (req, res) {
 	  				res.end(stdout);
 				}
 				exec("mpc stop", puts);
-
 			};
 			if (result[2] == "radio" & result[3] == "start") {
 				console.log("We should start the radio...");
@@ -34,10 +33,36 @@ http.createServer(function (req, res) {
 				}
 				exec("mpc play", puts);
 			};
+
+			if (result[2] == "volume") {
+				console.log("We should start the radio...");
+				var sys = require('sys')
+				var exec = require('child_process').exec;
+				function puts(error, stdout, stderr) {
+				 	sys.puts(stdout);
+				 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	  				res.end(stdout);
+				}
+				exec("mpc volume "+result[3], puts);
+			};
+
+			if (result[2] == "station") {
+				console.log("Change station...");
+				var sys = require('sys')
+				var exec = require('child_process').exec;
+				function puts(error, stdout, stderr) {
+				 	sys.puts(stdout);
+				 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	  				res.end(stdout);
+				}
+				exec("mpc clear;"+"mpc load "+result[3]+".pls"+";mpc play", puts);
+			};
+
 		}
+
 		else{
 					res.writeHead(200, {'Content-Type': 'text/plain'});
-	  				res.end("Bad arguments!");
+	  				res.end("Bad number of arguments!");
 		}
 
 	}
